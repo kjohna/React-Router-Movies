f6350b22a059275f3a60d06a2e3be0c61353d9e2
kjohna <43828763+kjohna@users.noreply.github.com>
2019-01-22 07:39:39 -0700

    Update MovieCard, each movie is a link

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const MovieCard = props => {
-  return;
+  return(
+    <Link to="/movies/id:">
+      Movie Card Link
+    </Link>
+  );
 };
 
 export default MovieCard;
