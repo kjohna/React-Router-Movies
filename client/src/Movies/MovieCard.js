ced4adaa17e29721995c5e571ab54e5b21ebfd8f
kjohna <43828763+kjohna@users.noreply.github.com>
2019-01-22 17:13:12 -0700

    create MovieCard, factor out of MovieList..more DRY

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
 
 const MovieCard = props => {
-  return(
-    <Link to="/movies/id:">
-      Movie Card Link
-    </Link>
+  return (
+    <div className="movie-card">
+      <h2>{props.movie.title}</h2>
+      <div className="movie-director">
+        Director: <em>{props.movie.director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{props.movie.metascore}</strong>
+      </div>
+      <h3>Actors</h3>
+
+      {props.movie.stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+    </div>
   );
 };
 
